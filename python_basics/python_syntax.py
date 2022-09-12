f6b7aa398bce57c2bb60420a0da2aa92192248c6
Şevval İLHAN <77413445+ilhansevval@users.noreply.github.com>
2022-09-12 20:34:36 +0300

    python_basics

---
+++ python_basics/python_syntax.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 12 20:06:58 2022
+
+@author: ilhan
+"""
+
+#python büyük küçük harfe duyarlı bir dildir.
+
+degisken = 3
+
+print(Degisken) #şeklinde çağırıldığında hata verecektir.
+
+# işareti ile yorum satırı yapılır.
+
+"""
+ayrıca bu yöntem de yorum/açıklama satırı yapmak için sıklıkla kullanılır.
+""" 
+
+#pythonda if else statementlar, döngüler tanımlanırken bir girinti bırakılmalıdır.
+
+a=3
+b=10
+if (a<b):
+    print("a, b'den küçüktür.")
+else:
+    print("a, b'den büyüktür.")
+    #print için bir tab boşluk bırakılmazsa hata verecektir.
+    
+
+#key words
+
+#print, int, float, def gibi yapılar değişken ismi olarak kullanılamazlar.
+
+def =1
+#def, definition'ın kısaltmasıdır.
+#definition ile python'da fonksiyon tanımlanır.
+#def yazıldığında python fonksiyon tanımlanacağını düşünüp invalid syntax hatası verir.
+
+
+#sayı ile değişken tanımlama
+
+degisken1 = 3
+degisken2 = 5
+
+1degisken = 7 
+#değişken tanımlanırken, değişken adının arasında ya da sonunda sayı olabilir.
+#ancak değişken adı sayı ile başlayamaz, invalid syntax hatası verir.
